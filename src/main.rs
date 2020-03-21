1f47c9136a15c23bbf15f00df820a9b661d5652d
Aaron Lee <alee@aaronosaur.us>
2020-03-21 17:55:12 -0500

    import all of the library crate, but still explain calling stuff in the library directly

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // We have to specify the crate name as if it was a different create, even though it's in our lib.rs
-use import_of_call::dock;
+use import_of_call::*;
 
 // Gives us access to the contents of tiki_bar.rs
 mod tiki_bar;
@@ -12,7 +12,9 @@ fn main() {
     // Here we can use `dock` because of the `use import_of_call::dock` statement above
     dock::supplies::restock();
 
-    // We have to scope this to the create since it lives in the library crate.
+    // This is callable because we import everything within the library create using `use import_of_call::*;`
+    eat_at_restaurant();
+    // We could also call the same function in this way, without having made the import first(but we probably wouldn't do it that way :).
     import_of_call::eat_at_restaurant();
 
     // This is fully scoped because it's in the library crate, and this is the binary crate.

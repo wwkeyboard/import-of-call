501c9a30b350ae438783ab9748509688b28c638e
Aaron Lee <alee@aaronosaur.us>
2020-03-23 19:49:01 -0500

    call a method from a sibling

--- src/restaurant/grill.rs
+++ src/restaurant/grill.rs
@@ -1,3 +1,9 @@
 pub fn take_order() {
     println!("thanks")
 }
+
+pub mod cook {
+    pub fn pears() {
+        println!("grilled pears are great!");
+    }
+}
--- src/restaurant/serving.rs
+++ src/restaurant/serving.rs
@@ -1,3 +1,10 @@
+use crate::restaurant::grill::*;
+
 pub fn take_order() {
     println!("thank you for your order");
+}
+
+pub fn desert() {
+    // `use crate::restaurant::grill::*;` brought this into scope
+    cook::pears();
 }
